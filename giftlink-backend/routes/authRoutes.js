01c6aa8c8805e80380ad2d30acdaacab50f79ac2
Wilson Bracamonte <wd.bracamonte10@gmail.com>
2026-02-17 20:53:50 -0500

    Implemented API endpoints

--- giftlink-backend/routes/authRoutes.js
+++ giftlink-backend/routes/authRoutes.js
@@ -54,4 +54,45 @@ router.post("/register", async (req, res) => {
     return res.status(500).send("Internal server error");
   }
 });
+
+router.post("/login", async (req, res) => {
+  try {
+    // Task 1: Connect to `giftsdb` in MongoDB through `connectToDatabase` in `db.js`.
+    const db = await connectToDatabase();
+    // Task 2: Access MongoDB `users` collection
+    const collection = db.collection("users");
+    // Task 3: Check for user credentials in database
+    const user = await collection.findOne({ email: req.body.email });
+    // Task 4: Task 4: Check if the password matches the encrypyted password and send appropriate message on mismatch
+    if (theUser) {
+      let result = await bcryptjs.compare(req.body.password, theUser.password);
+      if (!result) {
+        logger.error("Passwords do not match");
+        return res.status(404).json({ error: "Wrong pasword" });
+      }
+      //continue other tasks
+    }
+    // Task 5: Fetch user details from database
+    const userName = user.firstName;
+    const userEmail = user.email;
+    // Task 6: Create JWT authentication if passwords match with user._id as payload
+
+    let payload = {
+      user: {
+        id: theUser._id.toString(),
+      },
+    };
+    jwt.sign(user._id, JWT_SECRET);
+    res.json({ authtoken, userName, userEmail });
+    // Task 7: Send appropriate message if user not found
+    if (theUser) {
+      // Tasks 1-6 as done previously
+    } else {
+      logger.error("User not found");
+      return res.status(404).json({ error: "User not found" });
+    }
+  } catch (e) {
+    return res.status(500).send("Internal server error");
+  }
+});
 module.exports = router;
