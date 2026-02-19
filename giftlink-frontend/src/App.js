650985ca2e56d920fbc3214af5d5d1bd96b21292
Wilson Bracamonte <wd.bracamonte10@gmail.com>
2026-02-18 20:44:50 -0500

    fix

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route, useNavigate } from "react-router-dom";
+import { Routes, RouteWW } from "react-router-dom";
 import MainPage from "./components/MainPage/MainPage";
 import "bootstrap/dist/css/bootstrap.min.css";
 
